31920c73480d8307b626197cbbc01b4c6151348d
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-13 12:46:25 +0300

    feat: add title and description

--- client/src/components/home/Testimonial.jsx
+++ client/src/components/home/Testimonial.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import Title from "./Title";
+import { BookUserIcon } from "lucide-react";
 
 const Testimonial = () => {
   return (
     <div
       id="testimonials"
       className="flex items-center flex-col my-10 scroll-mt-12"
-    ></div>
+    >
+      <div className="flex items-center gap-2 text-sm text-green-800 bg-green-400/10  rounded-full px-6 py-1.5">
+        <BookUserIcon className="size-4.5 stroke-green-600" />
+        <span>Testimonials</span>
+      </div>
+      <Title
+        title="Don't just take our words"
+        description="Hear what our users say about us. We're always looking for ways to improve. If you have a positive experience with us, leave a review."
+      />
+    </div>
   );
 };
 
