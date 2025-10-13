5f0f4416605f927f748dcdad72b49f154c2e1002
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-13 12:51:28 +0300

    chore: add testimonial data

--- client/src/components/home/Testimonial.jsx
+++ client/src/components/home/Testimonial.jsx
@@ -3,6 +3,32 @@ import Title from "./Title";
 import { BookUserIcon } from "lucide-react";
 
 const Testimonial = () => {
+  const cardsData = [
+    {
+      image:
+        "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200",
+      name: "Briar Martin",
+      handle: "@neilstellar",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200",
+      name: "Avery Johnson",
+      handle: "@averywrites",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1527980965255-d3b416303d12?w=200&auto=format&fit=crop&q=60",
+      name: "Jordan Lee",
+      handle: "@jordantalks",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?w=200&auto=format&fit=crop&q=60",
+      name: "Avery Johnson",
+      handle: "@averywrites",
+    },
+  ];
   return (
     <div
       id="testimonials"
