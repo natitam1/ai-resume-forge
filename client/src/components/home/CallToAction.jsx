579046d435dc2b98187a2f512297a4c3ddc5b6a4
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-13 13:54:57 +0300

    chore: add margin

--- client/src/components/home/CallToAction.jsx
+++ client/src/components/home/CallToAction.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const CallToAction = () => {
   return (
-    <div className="border-y border-dashed border-slate-200 w-full max-w-5xl mx-auto px-10 sm:px-16">
+    <div
+      id="cta"
+      className="border-y border-dashed border-slate-200 w-full max-w-5xl mx-auto px-10 sm:px-16 mt-28"
+    >
       <div className="flex flex-col md:flex-row text-center md:text-left items-center justify-between gap-8 px-3 md:px-10 border-x border-dashed border-slate-200 py-16 sm:py-20 -mt-10 -mb-10 w-full">
         <p className="text-xl font-medium max-w-md text-slate-800">
           Star the repo and join the growing PrebuiltUI community.
