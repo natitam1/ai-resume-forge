78766133d778575684648442f9aa6f691009b49a
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-13 12:38:20 +0300

    chore: mount the testimonials

---
+++ client/src/components/home/Testimonial.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Testimonial = () => {
+  return (
+    <div
+      id="testimonials"
+      className="flex items-center flex-col my-10 scroll-mt-12"
+    ></div>
+  );
+};
+
+export default Testimonial;
