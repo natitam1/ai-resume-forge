df153308b2c629340d915f052c06904039af9a1c
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-13 14:02:55 +0300

    chore: update the button text and the description

--- client/src/components/home/CallToAction.jsx
+++ client/src/components/home/CallToAction.jsx
@@ -8,13 +8,13 @@ const CallToAction = () => {
     >
       <div className="flex flex-col md:flex-row text-center md:text-left items-center justify-between gap-8 px-3 md:px-10 border-x border-dashed border-slate-200 py-16 sm:py-20 -mt-10 -mb-10 w-full">
         <p className="text-xl font-medium max-w-md text-slate-800">
-          Star the repo and join the growing PrebuiltUI community.
+          Build a Professional Resume That Helps You Stand Out and Get Hired
         </p>
         <a
-          href="https://prebuiltui.com"
+          href="https://github.com/natitam1"
           className="flex items-center gap-2 rounded py-3 px-8 bg-green-600 hover:bg-green-700 transition text-white"
         >
-          <span>Star On Github</span>
+          <span>Get Started</span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
