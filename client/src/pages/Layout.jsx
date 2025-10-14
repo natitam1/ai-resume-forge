c53a770fa473c55130cade4682aba97c90eaba07
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-14 11:45:16 +0300

    feat: add navbar for the dashboard

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Layout = () => {
   return (
-    <div>
-      Layout
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
       <Outlet />
     </div>
   );
