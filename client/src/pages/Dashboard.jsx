77335249b99173279d9ed0d251a28e601effddce
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-14 13:36:41 +0300

    chore: set up resume adder

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { PlusIcon, UploadCloudIcon } from "lucide-react";
+import { dummyResumeData } from "../assets/assets";
 
 const Dashboard = () => {
+  const [allResumes, setAllResumes] = useState([]);
+
+  const loadAllResumes = async () => {
+    setAllResumes(dummyResumeData);
+  };
+
+  useEffect(() => {
+    loadAllResumes();
+  }, []);
   return (
     <div className="max-w-7xl mx-auto px-4 py-8">
       <p className="text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden">
