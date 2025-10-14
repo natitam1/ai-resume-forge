d04b11b15bf6be42342ce24a6bfe404878f31271
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-14 14:26:37 +0300

    feat: add all resumes dashboard UI

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { PlusIcon, UploadCloudIcon } from "lucide-react";
+import {
+  FilePenLine,
+  FilePenLineIcon,
+  PencilIcon,
+  PlusIcon,
+  TrashIcon,
+  UploadCloudIcon,
+} from "lucide-react";
 import { dummyResumeData } from "../assets/assets";
 
 const Dashboard = () => {
+  const colors = ["#9333ea", "#d97706", "#dc2626", "#0284c7", "#16a34a"];
   const [allResumes, setAllResumes] = useState([]);
 
   const loadAllResumes = async () => {
@@ -34,6 +42,43 @@ const Dashboard = () => {
       </div>
 
       <hr className="border-slate-300 my-6 sm:w-[305px]" />
+
+      <div className="grid grid-cols-2 sm:flex flex-wrap gap-4">
+        {allResumes.map((resume, index) => {
+          const baseColor = colors[index % colors.length];
+          return (
+            <button
+              key={index}
+              className="relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 border group hover:shadow-lg transition-all duration-300 cursor-pointer"
+              style={{
+                background: `linear-gradient(135deg, ${baseColor}10, ${baseColor}40)`,
+                borderColor: baseColor + "40",
+              }}
+            >
+              <FilePenLineIcon
+                className="size-7 group-hover:scale-105 transition-all"
+                style={{ color: baseColor }}
+              />
+              <p
+                style={{ color: baseColor }}
+                className="text-sm transition-all duration-300 group-hover:scale-105 px-2 text-center"
+              >
+                {resume.title}
+              </p>
+              <p
+                style={{ color: baseColor + "90" }}
+                className="absolute bottom-1 text-[11px] text-slate-400 transition-all duration-300 px-2 text-center"
+              >
+                Updated on {new Date(resume.updatedAt).toLocaleDateString()}
+              </p>
+              <div className="absolute top-1 right-1 group-hover:flex items-center hidden">
+                <TrashIcon className="size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors" />
+                <PencilIcon className="size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors" />
+              </div>
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 };
