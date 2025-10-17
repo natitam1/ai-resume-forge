b6f4ae0f2d32f91403fef92ba999b71764af562a
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-17 15:59:13 +0300

    chore: add state variables for the resume data

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ResumeBuilder = () => {
-  return <div>ResumeBuilder</div>;
+  const [resumeData, setResumeData] = useState({
+    _id: "",
+    title: "",
+    personal_info: {},
+    professional_summary: "",
+    experience: [],
+    education: [],
+    project: [],
+    skills: [],
+    template: "classic",
+    accent_color: "#3b82f6",
+    public: false,
+  });
+  return <div></div>;
 };
 
 export default ResumeBuilder;
