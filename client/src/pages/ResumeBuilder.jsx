48d43c6e5ff4dc13d0073dce98c610fc02d296a9
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-17 16:12:03 +0300

    chore: add back functionality

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { dummyResumeData } from "../assets/assets";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { ArrowLeftIcon } from "lucide-react";
 
 const { resumeId } = useParams();
 
@@ -22,12 +23,21 @@ const ResumeBuilder = () => {
   const loadExistingResume = async () => {
     const resume = dummyResumeData.find((resume) => resume._id === resumeId);
     setResumeData(resume);
+    document.title = resume.title;
   };
 
   useEffect(() => {
     loadExistingResume();
   }, []);
-  return <div></div>;
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto px-4 py-6">
+        <Link className="inline-flex gap-2 items-center transition-all text-slate-50 hover:text-slate-700">
+          <ArrowLeftIcon className="size-4" /> Back to Dashboard
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ResumeBuilder;
